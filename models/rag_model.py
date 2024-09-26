db39d6096ee435be6e24b35311cdd9882f10c4c8
254binaryninja <arnoldmusandu@gmail.com>
2024-09-26 19:24:27 +0300

    Refactor .gitignore and environment variable handling

--- models/rag_model.py
+++ models/rag_model.py
@@ -4,11 +4,13 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from pdfminer.high_level import extract_text
 from typing import BinaryIO, List, Dict, Any
+from dotenv import load_dotenv
+load_dotenv() 
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
 
-supabase_url = os.environ.get("SUPABASE_PROJECT_URL")
-supabase_key = os.environ.get("SUPABASE_API_KEY")
+supabase_url = os.getenv("SUPABASE_PROJECT_URL")
+supabase_key = os.getenv("SUPABASE_API_KEY")
 
 supabase_client = create_client(supabase_url, supabase_key)
 
